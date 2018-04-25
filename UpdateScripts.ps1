6acc252a6156e9d24d01554399f7fd1bbbd6172c
chasat-ms <chasat@microsoft.com>
2018-04-25 16:17:36 -0700

    Added UpdateScripts.ps1

---
+++ UpdateScripts.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.Synopsis
+
+Copy this script to the root of your drive and execute to:
+
+1. Pause scheduled tasks
+2. Update the scripts from the github repository
+3. Update any dependencies
+4. Enable the scheduled tasks
+
+#>
+#Disable Scheduled Tasks
+Disable-ScheduledTask -TaskName "UsageDataUpload1"
+Disable-ScheduledTask -TaskName "OperationalDataUpload1"
+
+#Clone Github repository into existing working directory
+cd C:\AZSAdminOMSInt
+git pull
+
+cd C:\AZSAdminOMSInt
+& .\UpdateDependencies.ps1
+
+#Enable Scheduled Tasks
+Enable-ScheduledTask -TaskName "UsageDataUpload1"
+Enable-ScheduledTask -TaskName "OperationalDataUpload1"
