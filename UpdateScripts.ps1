318486d3f500cf93bf535fed9d074628918456f2
chasat-ms <chasat@microsoft.com>
2018-04-25 16:20:06 -0700

    Added details of an error message on execution

--- UpdateScripts.ps1
+++ UpdateScripts.ps1
@@ -7,6 +7,18 @@ Copy this script to the root of your drive and execute to:
 2. Update the scripts from the github repository
 3. Update any dependencies
 4. Enable the scheduled tasks
+ 
+Note you may see the following error during execution
+git : From https://github.com/Azure-Samples/AzureStack-AdminPowerShell-OMSIntegration
+At C:\UpdateScripts.ps1:18 char:1
++ git pull
++ ~~~~~~~~
+    + CategoryInfo          : NotSpecified: (From https://gi...-OMSIntegration:String) [], RemoteException
+    + FullyQualifiedErrorId : NativeCommandError
+ 
+   3bf49bb..6acc252  master     -> origin/master
+   
+ Updates to the scripts will happen despite this error.
 
 #>
 #Disable Scheduled Tasks
