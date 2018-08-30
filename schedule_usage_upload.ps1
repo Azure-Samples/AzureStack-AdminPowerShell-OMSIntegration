8c993dfb2f4fa9709c3957334d5f9b81541fb16f
chasat-ms <chasat@microsoft.com>
2018-08-30 12:27:09 -0700

    Comment out Usage Upload
    Need to fix the usage upload scripts.  Commenting out till fixed.

--- schedule_usage_upload.ps1
+++ schedule_usage_upload.ps1
@@ -1,4 +1,5 @@
 #Usage Data Upload
+<# Removing Usage Upload until scripts fixed
 $action = New-ScheduledTaskAction -Execute 'Powershell' `
 -Argument '.\asUsageToOMS.ps1' -WorkingDirectory "C:\AZSAdminOMSInt"
 $description = "Daily upload of usage data from azure stack to OMS"
@@ -11,6 +12,7 @@ $twoHours = New-TimeSpan -Hour 2
 $settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit $twoHours
 
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Principal $principal -Description $description -Settings $settings
+#>
 
 #Operational Data Upload
 $action = New-ScheduledTaskAction -Execute 'Powershell' `
